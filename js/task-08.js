fddaf539aff0a7f2de2265b2c205c5cb7a4c5b8b
Nazakin <134816178+Nazakin@users.noreply.github.com>
2023-09-18 16:37:24 +0200

    bb

--- js/task-08.js
+++ js/task-08.js
@@ -1,15 +1,26 @@
-const email = document.getElementsByName('email');
-const password = document.getElementsByName('password');
-const form = document.querySelector('form');
+const form = document.querySelector('.login-form');
 
-const onSubmit = event => {
+  const onSubmit = (event) => {
     event.preventDefault();
-    if (email.value.length && password.value.length) {
-        console.log('succes');
-    }
-    else {
-        alert('Email or password is invalid');
-    }
-}
 
-form.addEventListener("submit", onSubmit)
+    const emailInput = form.elements.email;
+    const passwordInput = form.elements.password;
+
+    const emailValue = emailInput.value.trim(); 
+    const passwordValue = passwordInput.value.trim();
+
+    if (emailValue.length === 0 || passwordValue.length === 0) {
+      alert('Заповни всі поля!!!!!11!!11!11');
+    } else {
+      const formData = {
+        email: emailValue,
+        password: passwordValue,
+      };
+
+      console.log(formData);
+
+      form.reset();
+    }
+  };
+
+  form.addEventListener('submit', onSubmit);
